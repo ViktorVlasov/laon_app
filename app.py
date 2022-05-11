cdac748c6afb107a04eca33b68055ede6c5bc5af
efumiko <vlasov_viktor01@mail.ru>
2022-05-12 01:43:33 +0300

    add login form and hash password

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = 'replace later'
 
 # Configure database
-app.config['SQLALCHEMY_DATABASE_URI']=''
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://cbbydgxbphirfb:8561383db67f859da3fa51809c2d6a3f27970a8e6e46ccffe5a2a713ddf54b06@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dd6qsh2q8nbr1e'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -19,27 +19,32 @@ def index():
     if reg_form.validate_on_submit():
         username = reg_form.username.data
         password = reg_form.password.data
+        # Hash password
+        hashed_pswd = pbkdf2_sha256.hash(password)
 
-        user_object = User.query.filter_by(username=username).first()
-        if user_object:
-            return "Someone else has taken this username"
-
-        user = User(username=username, password=password)
+        # add user with hashed password to db
+        user = User(username=username, password=hashed_pswd)
         db.session.add(user)
         db.session.commit()
-        return "INSERT INTO DB"
-        # Hash password
-        # hashed_pswd = pbkdf2_sha256.hash(password)
-
-        # Add username & hashed password to DB
-        # user = User(username=username, hashed_pswd=hashed_pswd)
-        # db.session.add(user)
-        # db.session.commit()
 
         # flash('Registered successfully. Please login.', 'success')
-        # return redirect(url_for('login'))
+        return redirect(url_for('login'))
 
     return render_template("index.html", form=reg_form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+    login_form = LoginForm()
+
+    # Allow login if validation success
+    if login_form.validate_on_submit():
+        # user_object = User.query.filter_by(username=login_form.username.data).first()
+        # login_user(user_object)
+        return "Success logged!"
+        # return redirect(url_for('chat'))
+
+    return render_template("login.html", form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)

d81394cc2e4c8a81627f9df482eaf8a19cf41db6
efumiko <vlasov_viktor01@mail.ru>
2022-05-15 13:56:27 +0300

    login authetication functionality

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, redirect, url_for, request, render_template, session
+from flask import Flask, redirect, url_for, request, render_template, session, flash
+from flask_login import LoginManager, login_user, current_user, logout_user
+
+
 from wtform_fields import *
 from models import *
 
@@ -9,6 +12,14 @@ app.secret_key = 'replace later'
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://cbbydgxbphirfb:8561383db67f859da3fa51809c2d6a3f27970a8e6e46ccffe5a2a713ddf54b06@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dd6qsh2q8nbr1e'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# Initialize login manager
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -39,12 +50,31 @@ def login():
 
     # Allow login if validation success
     if login_form.validate_on_submit():
-        # user_object = User.query.filter_by(username=login_form.username.data).first()
-        # login_user(user_object)
-        return "Success logged!"
-        # return redirect(url_for('chat'))
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('chat'))
 
     return render_template("login.html", form=login_form)
+
+@app.route("/logout", methods=['GET'])
+def logout():
+
+    # Logout user
+    logout_user()
+    flash('You have logged out successfully', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+
+    if not current_user.is_authenticated:
+        flash('Please login', 'danger')
+        return redirect(url_for('login'))
+
+    return "Test card"
+    #return render_template("chat.html", username=current_user.username, rooms=ROOMS)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
